fc5e979effe7350e823cec489f64cb570b198d8a
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-07-07 14:15:20 +0300

    Config fullcalendar

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,7 +1,28 @@
-console.log('hello from index.js');
-function letStart() {
-  for (let i = 0; i <= 5; i++) {
-    console.log(`Time going ${i}`);
-  }
-}
-letStart();
+// Add calendar
+
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar');
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    // initialView: 'timeGridDay',
+    // initialView: 'listWeek',
+    themeSystem: 'standart',
+
+    selectable: true,
+    dateClick: function (date) {
+      console.log(date);
+    },
+    events: [
+      {
+        // this object will be "parsed" into an Event Object
+        title: 'Tommorow', // a property!
+        start: '2023-07-01', // a property!
+        end: '2023-07-03', // a property! ** see important note below about 'end' **
+      },
+    ],
+  });
+  calendar.render();
+  // calendar.on('dateClick', function (info) {
+  //   console.log(info);
+  // });
+});
